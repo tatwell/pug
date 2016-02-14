906db768f5929554b1c3f58ea3f4265861618800
tatwell <tatwell@gmail.com>
2016-02-14 10:28:09 -0800

    Adds fizzbuzz exercise solution.

---
+++ exercises/fizzbuzz.py
@@ -0,0 +1,70 @@
+"""
+FizzBuzz
+
+Write a program that prints the numbers from 1 to 100. But for multiples of three
+print "Fizz" instead of the number and for the multiples of five print "Buzz". For
+numbers which are multiples of both three and five print "FizzBuzz".
+
+Source: http://c2.com/cgi/wiki?FizzBuzzTest
+"""
+import sys
+
+
+def is_fizz(number):
+    return number % 3 == 0
+
+def is_buzz(number):
+    return number % 5 == 0
+
+def is_fizzbuzz(number):
+    return is_fizz(number) and is_buzz(number)
+
+def fizzbuzz_number(number):
+    if is_fizzbuzz(number):
+        return 'FizzBuzz'
+    elif is_fizz(number):
+        return 'Fizz'
+    elif is_buzz(number):
+        return 'Buzz'
+    else:
+        return number
+
+def fizzbuzz(n):
+    for n in range(1,n+1):
+        print fizzbuzz_number(n)
+
+def tests():
+    cases = [
+        # (number, expected)
+        (1, 1),
+        (3, 'Fizz'),
+        (5, 'Buzz'),
+        (15, 'FizzBuzz'),
+        (100, 'Buzz')
+    ]
+
+    for number, expected in cases:
+        assert fizzbuzz_number(number) == expected, "Failed for %s == %s" % (number, expected)
+
+    print 'Test passed for cases: %s' % (cases)
+
+def usage():
+    print """
+    USAGE
+    To run fizzbuzz 1 to 100.:
+        python fizzbuzz.py 100
+
+    To run tests:
+        python fizzbuzz.py test
+"""
+
+if __name__ == '__main__':
+    # Default: run help
+    request = sys.argv[1] if len(sys.argv) > 1 else 'help'
+
+    if request.isdigit():
+        fizzbuzz(int(request))
+    elif request == 'test':
+        tests()
+    else:
+        usage()
